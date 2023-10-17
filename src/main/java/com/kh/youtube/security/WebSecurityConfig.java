d1c0e2e8097f8d15c2e54575d6f5ddb13d52d6f8
Shin soomin <soomin6372@gmail.com>
2023-10-17 12:08:54 +0900

    feat_사진업로드 관련 설정

--- src/main/java/com/kh/youtube/security/WebSecurityConfig.java
+++ src/main/java/com/kh/youtube/security/WebSecurityConfig.java
@@ -20,6 +20,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+
         http.authorizeHttpRequests();
 
         http.cors()
